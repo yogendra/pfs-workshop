7a7e5312bd951a8de4950399f712b2bdb3d68574
Yogendra Rampuria (Yogi) <yrampuria@pivotal.io>
2019-06-24 21:26:04 +0800

    add get config

---
+++ setup-environment/get-config.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+
+source `dirname $0`/variables.sh
+
+SCRIPT_ROOT=$(cd  `dirname $0`; pwd)
+
+function get_config {
+
+    i=$1
+    user=user-$i
+    user_name=$user
+    namespace=$user_name-ns
+    config_file=$SCRIPT_ROOT/users/$user
+
+
+    user_token=$(kubectl get secret $(kubectl get sa $user_name -n ${namespace} -o json | jq -r .secrets[0].name) -n ${namespace} -o json | jq -r .data.token | base64 -d)
+
+    mkdir -p $user_root
+    cat <<EOF > $config_file
+apiVersion: v1
+clusters:
+- cluster:
+    certificate-authority-data: $kubernetes_ca
+    server: https://${kubernetes_host}:8443
+  name: workshop-cluster
+contexts:
+- context:
+    cluster: workshop-cluster
+    namespace: $namespace
+    user: $user_name
+  name: workshop
+current-context: workshop
+kind: Config
+preferences: {}
+users:
+- name: $user_name
+  user:
+    token: $user_token
+EOF
+}
+
+run get_config $*
