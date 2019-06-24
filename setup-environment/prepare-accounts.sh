0ad8c2369b06e068ddeaae8ab49e82eb04239355
Yogendra Rampuria (Yogi) <yrampuria@pivotal.io>
2019-06-24 18:35:14 +0800

    Update update to add view for knative-servinge

--- setup-environment/prepare-accounts.sh
+++ setup-environment/prepare-accounts.sh
@@ -1,41 +1,36 @@
 #!/usr/bin/env bash
 
-image_prefix=gcr.io/pa-yrampuria
+source `dirname $0`/variables.sh
 
-kubernetes_host=pfs-workshop-k8s.atwater.cf-app.com
-
-kubernetes_ca=Ci0tLS0tQkVHSU4gQ0VSVElGSUNBVEUtLS0tLQpNSUlDK3pDQ0FlT2dBd0lCQWdJVVdwS1hIVGtwdWsvMyt1Rk51ZG1MQ3dET25hOHdEUVlKS29aSWh2Y05BUUVMCkJRQXdEVEVMTUFrR0ExVUVBeE1DWTJFd0hoY05NVGt3TmpFek1EYzFOVFE0V2hjTk1qTXdOakV6TURjMU5UUTQKV2pBTk1Rc3dDUVlEVlFRREV3SmpZVENDQVNJd0RRWUpLb1pJaHZjTkFRRUJCUUFEZ2dFUEFEQ0NBUW9DZ2dFQgpBTUtOMmg1SmY5ZHVKN0NwOWVhR1RBN3lPQmlLWE5UZ09ucmRMQWhDZ1pUKzY5dmRsN2M1b1UvcHp3eldVMmlkCno3YmUvVVo0ZGFqQWlPbXJGaFZUTWd4UUdvT0ExMUdlRlU2elAyTmFtNld4enEwb1ZMeXcyeHdnanFBK3lPa2IKLzJLWnRjVVVhdE8xM2VNeDdMb1BlRTZ5SnAzVWxCb0tYN3pGSERvZEtVMjJoUHQ3cEJVajdEdG1CcWtETmpWWQozaHo5Y0gwNzR2V1UxaUFDVkRKNGd3NUI4dVVhVkNNY2hsSnhjYnNFVkRMdW5qSngrT0RYbk94bWtGZUdJUFVnClB4d3gzQUlTUHJyWS9LdG1JOGg5bmxFUlZFdnJYZ0Y1eEhDWXp0UGV4ZzRsNW11U3h5bzdsM291MGpnb3d4ZGgKVUxVNTZNS0lmU2V2MFRxSERPRmlFeWtDQXdFQUFhTlRNRkV3SFFZRFZSME9CQllFRkFOWEh3MGJPWFBsYmNBNApFSWVSNmJiRDAzVS9NQjhHQTFVZEl3UVlNQmFBRkFOWEh3MGJPWFBsYmNBNEVJZVI2YmJEMDNVL01BOEdBMVVkCkV3RUIvd1FGTUFNQkFmOHdEUVlKS29aSWh2Y05BUUVMQlFBRGdnRUJBSzNsMGY4czU2UWJLWG1nbENkUkpIL2MKVmZ0VDcxeTVkVzVlRnhHaFk4ZWJHaHMyYnFKTnk1VWlEd3J2cDllVUYwZXlybXNOOEk3cHBLSHVGbHUxcllRbApieU92Q0tkTXQvZGlMNytkRWxLZXppd2VBM0JxNjN1cEdOMVJiYnBmTE9TT3Bvd1NYTHA5RVE0bFlJQXBxSkVaCjV4UkJLc2Jyb1FZb1JSc0VoRHcwZTc2TVBheDJHY3pkdjh0Y1NWajV4SUJEbVNBblJtclRUK09iODY5aUhMSUUKWEJ0NnQ1WEVLdXdWTytaZC9zVlRxbkZ5cE1WaVJoMnBxTVZIc0ZISUhRa1NaaGQxRFVyeHBBKzhjNnVTdUJuNQoxb29kaEJFOXpFTjhHaER0b0gweW9Fcm1URXR5S3VZbEZPaytKQzV6b2VDeUIyVFhmTFVqSE1UdCtjOVpnK289Ci0tLS0tRU5EIENFUlRJRklDQVRFLS0tLS0K
-
-users_start=01
-users_end=99
+SCRIPT_ROOT=$(cd  `dirname $0`; pwd)
 
 
-SCRIPT_ROOT=$(cd `dirname $0` ; pwd)
+function prepare_account {
+  user=user-$i
+  namespace=${user}-ns
+  rolebinding=${user_name}-rb
 
+  user_root=$SCRIPT_ROOT/users/$user
+  config_file=$user_root/config
 
-eval "echo {$users_start..$users_end}" | tr ' ' \\n | while read i 
-do
+  user_name=$user
+  ns_user_name=${namespace}:${user_name}
 
-user=user-$i
-namespace=${user}-ns
+  # Create namespace
+  kubectl create ns ${namespace}
+  # Create user
+  kubectl create sa ${user_name} -n ${namespace}
+  # Create rolebinding under user ns
+  kubectl create rolebinding ${rolebinding} --serviceaccount=${ns_user_name} --clusterrole=admin -n ${namespace}
+  # Create rolebinding under default ns
+  kubectl create rolebinding ${rolebinding} --serviceaccount=${ns_user_name} --clusterrole=admin -n default
+  # Create rolebinding under knative service
+  kubectl create rolebinding ${rolebinding} --serviceaccount=${ns_user_name} --clusterrole=view -n knative-serving
 
-user_root=$SCRIPT_ROOT/users/$user
-config_file=$user_root/config
+  user_token=$(kubectl get secret $(kubectl get sa $user_name -n ${namespace} -o json | jq -r .secrets[0].name) -n ${namespace} -o json | jq -r .data.token | base64 -d)
 
-# Create user
-# Create namespace
-
-
-user_name=$user
-
-kubectl create ns ${namespace}
-kubectl create sa ${user_name} -n ${namespace}
-kubectl create rolebinding ${user_name}-rb --clusterrole=admin --serviceaccount=${namespace}:${user_name} -n ${namespace}
-kubectl create rolebinding ${user_name}-rb --clusterrole=admin --serviceaccount=${namespace}:${user_name} -n default
-user_token=$(kubectl get secret $(kubectl get sa $user_name -n ${namespace} -o json | jq -r .secrets[0].name) -n ${namespace} -o json | jq -r .data.token | base64 -d)
-
-mkdir -p $user_root
-cat <<EOF > $config_file
+  mkdir -p $user_root
+  cat <<EOF > $config_file
 apiVersion: v1
 clusters:
 - cluster:
@@ -57,10 +52,12 @@ users:
     token: $user_token
 EOF
 
-pfs --kubeconfig $config_file namespace init --image-prefix $image_prefix/$namespace --namespace $namespace
+  pfs --kubeconfig $config_file namespace init --image-prefix $image_prefix/$namespace --namespace $namespace
 
 
-echo Finished creating user:$user ns:$namespace and config:$config_file
-done
+  echo Finished creating user:$user ns:$namespace and config:$config_file
+}
 
 
+
+run prepare_account $*
