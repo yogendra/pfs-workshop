5c7109bc6fa134a4313ae3fa2364c23515ba1d6c
Yogendra Rampuria (Yogi) <yrampuria@pivotal.io>
2019-06-25 00:32:57 +0800

    FIx role mapping

--- setup-environment/prepare-accounts.sh
+++ setup-environment/prepare-accounts.sh
@@ -18,46 +18,28 @@ function prepare_account {
 
   # Create namespace
   kubectl create ns ${namespace}
+  
   # Create user
   kubectl create sa ${user_name} -n ${namespace}
-  # Create rolebinding under user ns
-  kubectl create rolebinding ${rolebinding} --serviceaccount=${ns_user_name} --clusterrole=admin -n ${namespace}
-  # Create rolebinding under default ns
-  kubectl create rolebinding ${rolebinding} --serviceaccount=${ns_user_name} --clusterrole=admin -n default
-  # Create rolebinding under knative service
-  kubectl create rolebinding ${rolebinding} --serviceaccount=${ns_user_name} --clusterrole=view -n knative-serving
-
-  user_token=$(kubectl get secret $(kubectl get sa $user_name -n ${namespace} -o json | jq -r .secrets[0].name) -n ${namespace} -o json | jq -r .data.token | base64 -d)
-
-  mkdir -p $user_root
-  cat <<EOF > $config_file
-apiVersion: v1
-clusters:
-- cluster:
-    certificate-authority-data: $kubernetes_ca
-    server: https://${kubernetes_host}:8443
-  name: workshop-cluster
-contexts:
-- context:
-    cluster: workshop-cluster
-    namespace: $namespace
-    user: $user_name
-  name: workshop
-current-context: workshop
-kind: Config
-preferences: {}
-users:
-- name: $user_name
-  user:
-    token: $user_token
-EOF
-
-  pfs --kubeconfig $config_file namespace init --image-prefix $image_prefix/$namespace --namespace $namespace
+  
+  # Add permisssion
+  kubectl create rolebinding ${user}-rb --serviceaccount=${ns_user_name} --clusterrole=cluster-admin -n ${namespace}
+  kubectl create rolebinding ${user}-rb --serviceaccount=${ns_user_name} --clusterrole=cluster-admin -n default
+  kubectl create rolebinding ${user}-rb --serviceaccount=${ns_user_name} --clusterrole=cluster-admin -n knative-build
+  kubectl create rolebinding ${user}-rb --serviceaccount=${ns_user_name} --clusterrole=cluster-admin -n knative-eventing
+  kubectl create rolebinding ${user}-rb --serviceaccount=${ns_user_name} --clusterrole=cluster-admin -n knative-serving
+  kubectl create rolebinding ${user}-rb --serviceaccount=${ns_user_name} --clusterrole=cluster-admin -n kube-system
+  
+  ns_image_prefix=$image_prefix/$user
 
 
-  echo Finished creating user:$user ns:$namespace and config:$config_file
+  echo pfs namespace init \
+    --kubeconfig $KUBECONFIG \
+    --image-prefix $ns_image_prefix \
+    -m $PWD/pfs-download/manifest.yaml \
+    --gcr $gcr_json \
+    $namespace
 }
-
 
 
 run prepare_account $*
